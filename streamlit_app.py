724262f3bdb00a33e8620cec014b1851d3b04462
wylliams <76174006+Kozato01@users.noreply.github.com>
2022-10-22 16:41:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,9 @@ fruits_show = my_fruit2.loc[fruits_select]
 streamlit.dataframe(fruits_show)
 
 #New scena
-streamlit.header('Fruityvice Fruit Advice!')
 import requests
-
+streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/kiwi")
-
-fruityvice_normalized = pd.json_normalize(fruityvice_normalized)
+fruityvice_normalized = pd.json_normalize(fruityvice_repsonse.json())
 
 streamlit.dataframe(fruityvice_normalized)
